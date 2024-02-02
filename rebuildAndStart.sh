f6bc0435d34e76ccdbac2d6476e6499f3059011f
Park-MinJe <phone7101@naver.com>
2024-02-02 15:54:58 +0900

    [수정] Update Shell Scripts and delete Program.cs file at remote

---
+++ rebuildAndStart.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Define the target port
+TARGET_PORT=5001
+
+# Check if the port is in use
+if lsof -Pi :$TARGET_PORT -sTCP:LISTEN -t &> /dev/null; then
+    echo "Port $TARGET_PORT is in use."
+
+    # Find and kill the process using the target port
+    PID=$(lsof -Pi :$TARGET_PORT -sTCP:LISTEN -t)
+    echo "Killing process with PID: $PID"
+    kill -9 $PID
+
+    echo "Process killed successfully."
+else
+    echo "Port $TARGET_PORT is not in use."
+fi
+
+# 디렉토리 경로
+DIR_PATH="artefacts/"
+
+# "artefacts/" 디렉토리가 존재하는지 확인
+if [ -d "$DIR_PATH" ]; then
+    # 디렉토리가 존재하면 삭제
+    rm -r "$DIR_PATH"
+    echo "Directory '$DIR_PATH' deleted successfully."
+else
+    # 디렉토리가 존재하지 않으면 메시지 출력
+    echo "Directory '$DIR_PATH' does not exist."
+fi
+
+powershell -File ./src/BuildForRelease.ps1
+sleep 5
+cd ./artefacts/build/Release/AasxServerBlazor
+chmod +x startForDemo.sh
+./startForDemo.sh
--- src/AasxServerBlazor/startForDemo.sh
+++ src/AasxServerBlazor/startForDemo.sh
@@ -1,4 +1,19 @@
 cd
 cd ./OSSPs/aasx-server-2023-11-06/artefacts/build/Release/AasxServerBlazor/
 nohup dotnet AasxServerBlazor.dll --rest --no-security --data-path ./aasxs --with-db 2>&1 & $OPTIONSAASXSERVER
-tail -f nohup.out
+cd
+cd ./OSSPs/aasx-server-2023-11-06/artefacts/build/Release/AasxServerBlazor/
+
+# 파일 경로 및 파일명
+LOG_FILE="nohup.out"
+
+# 대기 시간 (초)
+WAIT_TIME=3
+
+# 파일이 생성될 때까지 기다림
+while [ ! -f "$LOG_FILE" ]; do
+    sleep $WAIT_TIME
+done
+
+# 파일이 생성되면 tail 명령 실행
+tail -f "$LOG_FILE"
